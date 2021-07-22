a63e239912b03c91705a715866440f48bef6819b
suphasonnuk <suphasonnuk@gmai.com>
2021-07-22 17:35:01 +0700

    update contract

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -1,12 +1,10 @@
 import { ChainId } from '@pancakeswap-libs/sdk'
 import MULTICALL_ABI from './abi.json'
 
-let rinkeby = 4;
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
   [ChainId.BSCTESTNET]: '0x41857071aA165cBC7cC6Aa96ec37e3cDD09C772c',
-  [rinkeby]: '0x4dCa8b0ae136e578Ec8B0F1dDBC457A074705b12'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
--- src/constants/v1/index.ts
+++ src/constants/v1/index.ts
@@ -3,12 +3,10 @@ import { ChainId } from '@pancakeswap-libs/sdk'
 import V1_EXCHANGE_ABI from './v1_exchange.json'
 import V1_FACTORY_ABI from './v1_factory.json'
 
-let rinkeby = 4;
 
 const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0xC07d4604400139108BbdB3076636365A385879eF', // TODO
-  [ChainId.BSCTESTNET]: '0xc677E993abB6007174380559c451405186118C61',
-  [rinkeby]: '0x5046e92b9E52CB55ca7c2dDE712C6E58493FcF9d'
+  [ChainId.BSCTESTNET]: '0xc677E993abB6007174380559c451405186118C61'
 }
 
 const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
